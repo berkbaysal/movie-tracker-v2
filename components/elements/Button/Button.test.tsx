8e9dc4a6bf4fa51e68ac1d4445ba5340d2186841
berkbaysal <berkbaysal@gmail.com>
2022-11-21 15:55:42 +0300

    Added test for button element

---
+++ components/elements/Button/Button.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Button from './Button';
+import '@testing-library/jest-dom';
+
+describe('Test Button Functionality', () => {
+  test('Button renders', () => {
+    render(<Button label="Test button" />);
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  test('Button has correct label', () => {
+    render(<Button label="Test button" />);
+    expect(screen.getByRole('button')).toHaveTextContent('Test button');
+  });
+});
+
+describe('Test Button Style', () => {
+  test('Button has correct class', () => {
+    render(<Button label="Test button" />);
+    expect(screen.getByRole('button')).toHaveClass('c-button');
+    expect(screen.getByRole('button')).not.toHaveClass('c-button--reverse');
+  });
+
+  test('Reverse button has correct class', () => {
+    render(<Button label="Test button" variant="reverse" />);
+    expect(screen.getByRole('button')).toHaveClass('c-button');
+    expect(screen.getByRole('button')).toHaveClass('c-button--reverse');
+  });
+});
