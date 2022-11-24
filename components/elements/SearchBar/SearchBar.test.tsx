c5113ae06ce30cc8102a747233eecb183472239d
berkbaysal <berkbaysal@gmail.com>
2022-11-24 15:48:54 +0300

    added a test for keyboard inputs

--- components/elements/SearchBar/SearchBar.test.tsx
+++ components/elements/SearchBar/SearchBar.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SearchBar from './SearchBar';
 import '@testing-library/jest-dom';
 
@@ -21,5 +22,12 @@ describe('Search Bar Functionality', () => {
     render(<SearchBar />);
     expect(screen.getByRole('searchbox')).toHaveClass('u-display-none@sm-down');
     expect(screen.getByRole('button')).toHaveClass('u-display-none@md-up');
+  });
+
+  test('Search bar is responsive to keyboard inputs', async () => {
+    render(<SearchBar />);
+    const input = screen.getByRole('searchbox');
+    await userEvent.type(input, 'This is a test message.');
+    expect(input).toHaveValue('This is a test message.');
   });
 });
