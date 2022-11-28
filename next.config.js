2452f43cc772ecb0205fd1d18e776992076f595d
berkbaysal <berkbaysal@gmail.com>
2022-11-28 12:50:02 +0300

    updated deviceSizes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['www.themoviedb.org', 'image.tmdb.org', 'img.youtube.com'],
+    deviceSizes: [350, 600, 640, 750, 828, 1080, 1200, 1440],
   },
   i18n: {
     locales: ['en'],
