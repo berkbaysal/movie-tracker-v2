6e6f64fb26304fed2280df43d9fccecf5f1bcbee
berkbaysal <berkbaysal@gmail.com>
2022-11-28 17:31:02 +0300

    Added meta information

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
+import Head from 'next/head';
 import '../scss/main.scss';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="application-name" content="movie-tracker" />
+        <meta
+          name="description"
+          content="Track the movies and tv shows you watched"
+        />
+        <meta name="keywords" content="film , movie, tv shows, tv series" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
