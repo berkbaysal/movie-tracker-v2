067702d1b5e6b365a63bcbce5a3ea511c67553ae
Berk Baysal <berkbaysal@gmail.com>
2023-11-11 17:08:01 +0300

    added main page metadata to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import Head from 'next/head';
 import '../scss/main.scss';
 import { Navbar, Footer } from '@components';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  applicationName: 'movie-tracker',
+  title: 'The Movie Tracker',
+  description: 'Track the movies and tv shows you watched',
+  keywords: 'film , movie, tv shows, tv series',
+  icons: '/img/logo-icon.svg',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <Head>
-        <meta charSet="UTF-8" />
-        <meta name="application-name" content="movie-tracker" />
-        <meta name="description" content="Track the movies and tv shows you watched" />
-        <meta name="keywords" content="film , movie, tv shows, tv series" />
+        <link href="./img/logo.svg" rel="icon" type="image/svg" />
       </Head>
       <body className="o-page-content-cotainer">
         <Navbar />
