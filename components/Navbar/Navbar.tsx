5454ebf574dc1645a319e1750d74a9ffe8c84488
Berk Baysal <berkbaysal@gmail.com>
2023-11-11 17:24:58 +0300

    added linking to navbar logo

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 import { Button } from '@elements';
 import Logo from '@public/img/logo.svg';
+import Link from 'next/link';
 import SearchBar from './SearchBar/SearchBar';
 
 function Navbar() {
@@ -10,7 +11,9 @@ function Navbar() {
       <div className="container">
         <div className="row">
           <div className="col  u-display-flex">
-            <Image src={Logo} alt="Movie tracker logo" className="c-navbar__logo" />
+            <Link href="/">
+              <Image src={Logo} alt="Movie tracker logo" className="c-navbar__logo" />
+            </Link>
             <div className="c-navbar__menu">
               <SearchBar />
               <Button label="Log In" role="link" />
