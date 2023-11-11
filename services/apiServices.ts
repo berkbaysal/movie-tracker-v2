a5c35b655a1abf31790cade159fe7857320bfe09
Berk Baysal <berkbaysal@gmail.com>
2023-11-11 17:41:55 +0300

    clean up services & types

--- services/apiServices.ts
+++ services/apiServices.ts
@@ -6,12 +6,20 @@ import axios from 'axios';
 
 dotenv.config();
 
+const params = {
+  api_key: process.env.MOVIE_DB_API_KEY,
+  language: 'en-US',
+};
+
 interface GetTrendingListParameters {
   limit?: number;
   period?: 'week' | 'day';
 }
 
-export function filterTrendingResults(fetchRes: TrendingResponse, maxResults: number): TrendingResult[] {
+export function filterTrendingResults(
+  fetchRes: PaginatedResponse<TrendingResponse>,
+  maxResults: number
+): TrendingResult[] {
   const formattedResults: TrendingResult[] = [];
   for (const result of fetchRes.results) {
     // Filter
@@ -43,43 +51,27 @@ export async function getTrendingList({ limit = 20, period = 'week' }: GetTrendi
   const maxResults = limit <= 20 ? limit : 20;
 
   // Define fetch parameters
-  const params = {
-    api_key: process.env.MOVIE_DB_API_KEY,
-    language: 'en-US',
-  };
 
   const fetchRes = await axios
-    .get<TrendingResponse>(`${apiURL}/trending/all/${period}`, { params })
+    .get<PaginatedResponse<TrendingResponse>>(`${apiURL}/trending/all/${period}`, { params })
     .then((res) => res.data);
   // Filter results to only include TV Shows and Films, format and trim to fit custom interface
   return filterTrendingResults(fetchRes, maxResults);
 }
 
 export async function getMovieInfo(id: number): Promise<MovieListResult> {
-  const params = {
-    api_key: process.env.MOVIE_DB_API_KEY,
-    language: 'en-US',
-  };
   const fetchRes = await axios.get<MovieListResult>(`${apiURL}/movie/${id}`, { params }).then((res) => res.data);
 
   return fetchRes;
 }
 
 export async function getMovieCredits(id: number): Promise<MovieCredits> {
-  const params = {
-    api_key: process.env.MOVIE_DB_API_KEY,
-    language: 'en-US',
-  };
   const fetchRes = await axios.get(`${apiURL}/movie/${id}/credits`, { params }).then((res) => res.data);
 
   return fetchRes;
 }
 
 export async function getMovieRecommendations(id: number): Promise<PaginatedResponse<MovieListResult>> {
-  const params = {
-    api_key: process.env.MOVIE_DB_API_KEY,
-    language: 'en-US',
-  };
   const fetchRes = await axios
     .get<PaginatedResponse<MovieListResult>>(`${apiURL}/movie/${id}/recommendations`, { params })
     .then((res) => res.data);
