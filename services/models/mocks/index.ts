e61f55626f0ec3163c4c5bbbfcb07ecfcc3adab7
Berk Baysal <berkbaysal@gmail.com>
2023-11-12 15:46:18 +0300

    created mock response files

---
+++ services/models/mocks/index.ts
@@ -0,0 +1,6 @@
+// movie
+export { default as mockMovieCreditsResponse } from './movie/MovieCreditsResponse.json';
+export { default as mockMovieRecommendationResponse } from './movie/MovieRecommendationResponse.json';
+export { default as mockMovieDetailResponse } from './movie/MovieDetailResponse.json';
+// trending
+export { default as mockTrendingResponse } from './trending/TrendingResponse.json';
