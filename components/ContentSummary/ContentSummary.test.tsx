2663f334809c0015fa93d32576d79ecfd82bb256
Berk Baysal <berkbaysal@gmail.com>
2023-11-26 18:35:02 +0300

    corrected test description

--- components/ContentSummary/ContentSummary.test.tsx
+++ components/ContentSummary/ContentSummary.test.tsx
@@ -11,7 +11,7 @@ jest.mock('axios');
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-describe('Footer Functionality', () => {
+describe('Content Summary Functionality', () => {
   afterEach(jest.clearAllMocks);
   let contentInfo: MediaContentDetails;
   let credits: MediaContentCredits;
