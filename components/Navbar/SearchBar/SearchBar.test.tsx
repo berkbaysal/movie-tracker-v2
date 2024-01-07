3f13e1fb4274d35fcf9a519c820712e8a565099e
Berk Baysal <berkbaysal@gmail.com>
2024-01-07 19:00:16 +0300

    corrected test implementation of search bar

--- components/Navbar/SearchBar/SearchBar.test.tsx
+++ components/Navbar/SearchBar/SearchBar.test.tsx
@@ -6,23 +6,23 @@ import '@testing-library/jest-dom';
 
 describe('Search Bar Functionality', () => {
   test('Search bar renders', () => {
-    render(<SearchBar />);
+    render(<SearchBar query="" setQuery={() => {}} />);
     expect(screen.getByRole('searchbox')).toBeInTheDocument();
   });
 
   test('Search bar has placeholder text', () => {
-    render(<SearchBar />);
+    render(<SearchBar query="" setQuery={() => {}} />);
     expect(screen.getByRole('searchbox')).toHaveProperty('placeholder', 'Search...');
   });
 
   test('Correct utility classes are applied for rendering search UI separately on mobile', () => {
-    render(<SearchBar />);
+    render(<SearchBar query="" setQuery={() => {}} />);
     expect(screen.getByRole('searchbox')).toHaveClass('u-display-none@sm-down');
     expect(screen.getByRole('button')).toHaveClass('u-display-none@md-up');
   });
 
   test('Search bar is responsive to keyboard inputs', async () => {
-    render(<SearchBar />);
+    render(<SearchBar query="" setQuery={() => {}} />);
     const input = screen.getByRole('searchbox');
     await userEvent.type(input, 'This is a test message.');
     expect(input).toHaveValue('This is a test message.');
