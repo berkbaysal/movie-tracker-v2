d6ca78d77eddf3142289bd31ec300a60c76543f9
Berk Baysal <berkbaysal@gmail.com>
2024-04-12 17:26:01 +0300

    corrected search bar story

--- components/Navbar/SearchBar/SearchBar.stories.tsx
+++ components/Navbar/SearchBar/SearchBar.stories.tsx
@@ -1,3 +1,4 @@
+import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 import '@styles';
 import SearchBar from './SearchBar';
@@ -5,8 +6,19 @@ import SearchBar from './SearchBar';
 export default {
   title: 'Sub-Components/Search Bar',
   component: SearchBar,
+  argTypes: {
+    setQuery: { action: 'set query', table: { disable: true } },
+    setResultsVisible: { action: 'set results visible', table: { disable: true } },
+  },
+} as Meta<typeof SearchBar>;
+
+export const Default: StoryObj<typeof SearchBar> = {
+  args: {
+    query: '',
+    setQuery: () => {},
+    setResultsVisible: () => {},
+  },
+  render: ({ query, setQuery, setResultsVisible }) => (
+    <SearchBar query={query} setQuery={setQuery} setResultsVisible={setResultsVisible} />
+  ),
 };
-
-const Template = () => <SearchBar />;
-
-export const Default = Template.bind({});
